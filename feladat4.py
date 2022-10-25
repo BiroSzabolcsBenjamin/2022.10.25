af552bca52f1f789f8bd89c3bb8f233fa24b8357
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2022-10-25 10:02:09 +0200

    F4

--- feladat4.py
+++ feladat4.py
@@ -1,4 +1,12 @@
 #Fej vagy írás
 import random
 
-input
+user=input("Fej vagy írás?:")
+
+
+pc=random.choice(["fej","írás","Fej","Írás"])
+
+if (user == pc):
+    print("Eltaláltad")
+else:
+    print("Nem találtad el")
